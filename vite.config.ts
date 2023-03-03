1bd7ff9292bb9bc9e404a117eb2e11e3864c9ee8
Mark P <mj.poso18@gmail.com>
2023-03-03 21:19:29 +0800

    Setup alias to `./src`

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { relToAbsPath } from './src/utilities/paths.js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      src: relToAbsPath('./src', import.meta.url),
+    },
+  },
 });
