458929b9d123e58a21d42f72c2c046229368381c
Mark P <mj.poso18@gmail.com>
2023-03-06 22:45:34 +0800

    Fix async sleep utility to use actual seconds

--- src/utilities/misc.ts
+++ src/utilities/misc.ts
@@ -1,6 +1,8 @@
 /**
  * Wait for `duration` seconds **asynchronously**.
+ *
+ * https://stackoverflow.com/a/39914235
  */
 export async function sleep(duration: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, duration));
+  return new Promise((resolve) => setTimeout(resolve, duration * 1000));
 }
