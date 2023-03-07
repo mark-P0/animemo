9c4d3d81dce5d3631111565b8a35f95f4e9b5fe6
Mark P <mj.poso18@gmail.com>
2023-03-07 23:28:18 +0800

    Place path utility in config file itself to avoid project reference issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
+import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { relToAbsPath } from './src/utilities/paths.js';
+
+/**
+ * Expand relative paths to absolute counterpart.
+ *
+ * Base must be manually specified to e.g. `import.meta.url` from the
+ * consuming script, as specifying it directly here will use the
+ * directory of _this_ script as the base.
+ *
+ * https://stackoverflow.com/questions/66043612/vue3-vite-project-alias-src-to-not-working
+ */
+function relToAbsPath(path: string, base: string) {
+  return fileURLToPath(new URL(path, base));
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
