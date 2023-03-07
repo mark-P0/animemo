d59389ebae559e4e502108343d9658d277e5796d
Mark P <mj.poso18@gmail.com>
2023-03-07 23:30:35 +0800

    Add base path to Vite config for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,16 @@ function relToAbsPath(path: string, base: string) {
 export default defineConfig({
   plugins: [react()],
 
+  /**
+   * Use relative base path so that assets will resolve to the
+   * repo path (e.g. <user>.github.io/<repo>) instead of the
+   * root URL (<user>.github.io)
+   *
+   * - https://vitejs.dev/config/shared-options.html#base
+   * - https://vitejs.dev/guide/static-deploy.html#github-pages
+   */
+  base: './',
+
   resolve: {
     alias: {
       src: relToAbsPath('./src', import.meta.url),
